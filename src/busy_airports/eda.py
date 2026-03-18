54e7bf1777e8c91d73b9b68fc3c3cee2e03c31ac
David Friedenberg <dfriede1@tamu.edu>
2026-03-18 15:08:13 -0600

    updated plotting methods for EDA

--- src/busy_airports/eda.py
+++ src/busy_airports/eda.py
@@ -1,3 +1,9 @@
+"""
+eda.py
+
+Plotting methods for the exploratory data analysis
+"""
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,21 +11,64 @@ import seaborn as sns
 
 #['datetime', 'airport_code', 'airport_name', 'city', 'state','checkpoint', 'total_passenger_throughput', 'date', 'month', 'week']
 
-def plot_hourly_by_airport_checkpoint(df, checkpoint="10", airport="ORD"):
-    df_query = df.query(f'airport_code == "{airport}" and checkpoint == "{checkpoint}"')
-    plt.figure(figsize=(15, 6))
-    plt.plot(df_query['datetime'], df_query['total_passenger_throughput'])
-    plt.title(f'Hourly TSA Throughput - {airport} Checkpoint {checkpoint}')
-    plt.xlabel('Date/Time')
-    plt.ylabel('Passenger Throughput')
-    plt.show()
+def plot_airport_data(df, airport="ORD", frequency='D', checkpoint="all"):
+    """
+    Plot the total passenger throughput data for the given airport(s).
 
-def plot_daily_by_airport_checkpoint(df, checkpoint="10", airport="ORD"):
-    df_query = df.query(f'airport_code == "{airport}" and checkpoint == "{checkpoint}"')
-    df_grouped = df_query.groupby(pd.Grouper(key='datetime', freq='D'))['total_passenger_throughput'].sum()
+    Parameters
+    ----------
+    df : pd.DataFrame
+        pandas DataFrame containing the TSA throughput time series data
+    airport : object
+        name of airport or list of airports to plot
+    frequency : str
+        frequency at which to sample time series data (daily, weekly, or monthly data).
+        If multiple frequencies are given, these are plotted together
+    checkpoint : str
+        name of airport checkpoint or list of checkpoints to plot
+    """
+
+    if frequency not in ['H', 'D', 'W', 'M']:
+        raise ValueError("Invalid frequency. Use 'H' for hourly, 'D' for daily, 'W' for weekly, or 'M' for monthly.")
+    if frequency == 'M':
+        frequency = 'MS'
+
+    # Normalize inputs
+    if isinstance(airport, str):
+        airports = [airport]
+    else:
+        airports = airport
+
+    freq_names = {'H': 'Hourly', 'D': 'Daily', 'W': 'Weekly', 'MS': 'Monthly'}
+
     plt.figure(figsize=(15, 6))
-    plt.plot(df_grouped.index, df_grouped)
-    plt.title(f'Daily TSA Throughput - {airport} Checkpoint {checkpoint}')
+
+    for ap in airports:
+        df_airport = df[df['airport_code'] == ap]
+        if checkpoint == "all":
+            df_query = df_airport.groupby(['datetime'], as_index=False).total_passenger_throughput.sum()
+        else:
+            df_query = df_airport[df_airport['checkpoint'] == checkpoint]
+        if frequency != 'H':
+            df_query = (
+                df_query
+                .groupby(pd.Grouper(key='datetime', freq=frequency))['total_passenger_throughput']
+                .sum()
+                .reset_index()
+            )
+        if frequency == 'W':
+            df_query = df_query[1:-1].reset_index(drop=True)
+        plt.plot(
+            df_query['datetime'],
+            df_query['total_passenger_throughput'],
+            label=ap
+        )
+    plt.title(
+        f'{freq_names[frequency]} TSA Throughput - ' +
+        (f'{ap}, ' if len(airports) == 1 else '') +
+        (f'Checkpoint {checkpoint}' if checkpoint != "all" else 'All Checkpoints')
+    )
     plt.xlabel('Date/Time')
     plt.ylabel('Passenger Throughput')
+    plt.legend()
     plt.show()
