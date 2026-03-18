57ce854623866e9308a376a63e1a5d2ee6b87e92
David Friedenberg <dfriede1@tamu.edu>
2026-03-18 12:58:26 -0600

    more cleanup and commenting of python sources

--- src/busy_airports/harmonic_model.py
+++ src/busy_airports/harmonic_model.py
@@ -1,7 +1,29 @@
+"""
+harmonic_model.py
+
+Functions for constructing design matrix for Harmonic regression
+"""
+
 import numpy as np
 
-# Create matrix of Fourier features for modeling seasonality
 def fourier_features(time_seres, m, k=10):
+    """
+    Create matrix of Fourier features for modeling seasonality
+
+    Parameters
+    ----------
+    time_series: object
+        time series index over which to create Fourier features
+    m: int
+        seasonality (periodicity of Fourier features)
+    k: int
+        number of Fourier frequencies to use
+    
+    Returns
+    -------
+    X : object
+        design matrix of shape (len(time_series), 2*k) containing Fourier features
+    """
     X = []
     for i in range(1, k + 1):
         X.append(np.sin(2 * np.pi * i * time_seres / m))
@@ -10,4 +32,17 @@ def fourier_features(time_seres, m, k=10):
 
 # Create row matrix of time index feature
 def time_index_feature(time_series):
+    """
+    Create row matrix of time index feature
+
+    Parameters
+    ----------
+    time_series: object
+        time series data over which to create time index feature
+    
+    Returns
+    -------
+    X : object
+        design matrix of shape (len(time_series), 1) containing overall time index
+    """
     return np.arange(len(time_series))
--- src/busy_airports/preprocessing.py
+++ src/busy_airports/preprocessing.py
@@ -1,9 +1,24 @@
-import pandas as pd
+"""
+preprocessing.py
 
+Functions for preprocessing the TSA throughput data before forecasting
+"""
+
+import pandas as pd
 
 def load_data(path):
     """
     Load dataset from CSV file and parse timestamp column.
+    
+    Parameters
+    ----------
+    path: str
+        path to CSV file containing TSA throughput time series data
+    
+    Returns
+    -------
+    df : pd.DataFrame
+        pandas DataFrame containing the TSA throughput data with appropriately parsed columns
     """
     df = pd.read_csv(path)
 
@@ -20,6 +35,23 @@ def load_data(path):
     return df
 
 def retrieve_airport_data(df, airport="ORD", frequency='W'):
+    """
+    Extract time series TSA throughput data for a single airport
+    
+    Parameters
+    ----------
+    df : pd.DataFrame
+        pandas DataFrame containing the TSA throughput data (obtained from load_data function)
+    airport: str
+        airport code of airport to extract time series data from (summed over all TSA checkpoints)
+    frequency: str
+        frequency at which to sample time series data (daily, weekly, or monthly data)
+    
+    Returns
+    -------
+    df : pd.DataFrame
+        pandas DataFrame containing the TSA throughput time series data for the given airport
+    """
 
     if frequency not in ['D', 'W', 'M']:
         raise ValueError("Invalid frequency. Use 'D' for daily, 'W' for weekly, or 'M' for monthly.")
@@ -27,7 +59,7 @@ def retrieve_airport_data(df, airport="ORD", frequency='W'):
     #Keep only dataframe of working airport
     df.drop(df[df['airport_code'] != airport].index, inplace=True)
 
-    #Drop cases of zero passangers
+    #Drop cases of zero passengers
     df.drop(df[df['total_passenger_throughput'] == 0].index, inplace=True)
 
     df['date'] = pd.to_datetime(df['datetime']).dt.date
@@ -47,4 +79,3 @@ def retrieve_airport_data(df, airport="ORD", frequency='W'):
         df['day'] = df['datetime'].dt.dayofyear.astype(int)
     
     return df
-
