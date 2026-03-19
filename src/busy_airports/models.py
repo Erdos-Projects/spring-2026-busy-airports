9deeead7a585b70c320b30664cf13c5eee60a21e
David Friedenberg <dfriede1@tamu.edu>
2026-03-19 09:54:15 -0600

    created template for finished models

---
+++ src/busy_airports/models.py
@@ -0,0 +1,44 @@
+"""
+models.py
+
+Forecasting models for TSA data
+"""
+
+import numpy as np
+import pandas as pd
+
+# Harmonic Regression with Daily Data
+class HarmonicDaily:
+    def __init__(self, season_length):
+        pass
+    def fit(self, ts):
+        pass
+    def forecast(self, h):
+        pass
+
+# Harmonic Regression with Weekly Data
+class HarmonicWeekly:
+    def __init__(self, season_length):
+        pass
+    def fit(self, ts):
+        pass
+    def forecast(self, h):
+        pass
+
+# STL model with Daily Data
+class STLDaily:
+    def __init__(self, season_length):
+        pass
+    def fit(self, ts):
+        pass
+    def forecast(self, h):
+        pass
+
+# STL model with Weekly Data
+class STLWeekly:
+    def __init__(self, season_length):
+        pass
+    def fit(self, ts):
+        pass
+    def forecast(self, h):
+        pass
