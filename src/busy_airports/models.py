386b6acda5550b2f8c1a0c805675d34c2c21e2e1
David Friedenberg <dfriede1@tamu.edu>
2026-03-19 12:39:02 -0600

    fixed small issues with harmonic ARIMA model

--- src/busy_airports/models.py
+++ src/busy_airports/models.py
@@ -12,30 +12,30 @@ from busy_airports.harmonic_model import fourier_features
 # Harmonic Regression model with daily throughput data
 class HarmonicARIMA:
 
-    def __init__(self, season_length, n_freq, arima_order, seasonal_order, with_intercept):
+    def __init__(self, season_length, n_freq, arima_order, seasonal_order, with_intercept=True):
         self.season_length = season_length
         self.n_freq = n_freq
         self.arima_order = arima_order
         self.seasonal_order = seasonal_order
         self.arima_trend = ('c' if with_intercept else 'n')
     
-    def fit(self, ts):
-        X_fourier = fourier_features(ts, m = self.season_length, k = self.n_freq)
-        sarimax_model = SARIMAX(ts,
+    def fit(self, ts_index, ts_data, maxiter=50):
+        X_fourier = fourier_features(ts_index, m = self.season_length, k = self.n_freq)
+        sarimax_model = SARIMAX(ts_data,
                                 exog = X_fourier,
                                 order = self.arima_order,
                                 seasonal_order = self.seasonal_order,
                                 trend = self.arima_trend)
-        self.fitted_model = sarimax_model.fit()
+        self.fitted_model = sarimax_model.fit(maxiter=maxiter)
     
-    def forecast(self, ts):
-        X_fourier = fourier_features(ts, m = self.season_length, k = self.n_freq)
-        results = self.fitted_model.get_forecast(steps = len(ts), exog = X_fourier)
+    def forecast(self, ts_index):
+        X_fourier = fourier_features(ts_index, m = self.season_length, k = self.n_freq)
+        results = self.fitted_model.get_forecast(steps = len(ts_index), exog = X_fourier)
         return results.predicted_mean
     
-    def conf_int(self, ts, alpha=0.05):
-        X_fourier = fourier_features(ts, m = self.season_length, k = self.n_freq)
-        results = self.fitted_model.get_forecast(steps = len(ts), exog = X_fourier)
+    def conf_int(self, ts_index, alpha=0.05):
+        X_fourier = fourier_features(ts_index, m = self.season_length, k = self.n_freq)
+        results = self.fitted_model.get_forecast(steps = len(ts_index), exog = X_fourier)
         return results.conf_int(alpha=0.05)
 
 # STL model with Daily Data
